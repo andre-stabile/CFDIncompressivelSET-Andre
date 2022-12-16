b6a1e9333a27a878390db03a60f22c209fb3ff29
andre-stabile <andrestabile@usp.br>
2022-12-16 16:52:23 -0300

    Changes on different time steps consideration

--- src/Fluid.hpp
+++ src/Fluid.hpp
@@ -1605,21 +1605,19 @@ int Fluid<2>::solveTransientProblem(int iterNumber, double tolerance) {
         };
     
         //Start the analysis with first order time integration and then change to the user defined
-        if (integScheme < 1.){
-            double one = 1.;
-            if (iTimeStep == 0){
-                for (int i = 0; i < numElem; i++){
-                    elements_[i] -> setTimeIntegrationScheme(one);
-                    dTime = firstTimeStep_; //passo de tempo inicial
-                    elements_[i] -> setTimeStep(dTime);
-                };
+        double one = 1.;
+        if (iTimeStep == 0 && (integScheme < 1. || dTimeAux != firstTimeStep_)){
+            for (int i = 0; i < numElem; i++){
+                elements_[i] -> setTimeIntegrationScheme(one);
+                dTime = firstTimeStep_; //passo de tempo inicial
+                elements_[i] -> setTimeStep(dTime);
             };
-            if (iTimeStep == initialStructuralTimeStep_) {
-                for (int i = 0; i < numElem; i++){
-                    elements_[i] -> setTimeIntegrationScheme(integScheme);
-                    dTime = dTimeAux; //passo de tempo permanente
-                    elements_[i] -> setTimeStep(dTime);
-                };
+        };
+        if (iTimeStep == initialStructuralTimeStep_) {
+            for (int i = 0; i < numElem; i++){
+                elements_[i] -> setTimeIntegrationScheme(integScheme);
+                dTime = dTimeAux; //passo de tempo permanente
+                elements_[i] -> setTimeStep(dTime);
             };
         };
 
@@ -1996,21 +1994,23 @@ int Fluid<2>::solveTransientProblemMoving(int iterNumber, double tolerance) {
         };
 
         //Start the analysis with first order time integration and then change to the user defined
-        if (integScheme < 1.){
-            double one = 1.;
-            if (iTimeStep == 0){
-                for (int i = 0; i < numElem; i++){
-                    elements_[i] -> setTimeIntegrationScheme(one);
-                    dTime = firstTimeStep_; //passo de tempo inicial
-                    elements_[i] -> setTimeStep(dTime);
-                };
+        double one = 1.;
+        if (iTimeStep == 0 && (integScheme < 1. || dTimeAux != firstTimeStep_)){
+            for (int i = 0; i < numElem; i++){
+                elements_[i] -> setTimeIntegrationScheme(one);
+                dTime = firstTimeStep_; //passo de tempo inicial
+                elements_[i] -> setTimeStep(dTime);
             };
-            if (iTimeStep == initialStructuralTimeStep_) {
-                for (int i = 0; i < numElem; i++){
-                    elements_[i] -> setTimeIntegrationScheme(integScheme);
-                    dTime = dTimeAux; //passo de tempo permanente
-                    elements_[i] -> setTimeStep(dTime);
-                };
+        };
+        if (iTimeStep == initialStructuralTimeStep_) {
+            dTime = dTimeAux; //passo de tempo permanente
+            for (int i = 0; i < numElem; i++){
+                elements_[i] -> setTimeStep(dTime);
+            };
+        };
+        if (iTimeStep == initialStructuralTimeStep_ + 10) {
+            for (int i = 0; i < numElem; i++){
+                elements_[i] -> setTimeIntegrationScheme(integScheme);
             };
         };
 
